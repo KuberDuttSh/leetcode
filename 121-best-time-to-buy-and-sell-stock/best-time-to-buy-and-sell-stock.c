77a684adfe15c9137e39fb7022b4d8d8b4dd219a
Kuber Dutt Sharma <kuberduttsh@gmail.com>
2026-05-28 23:10:03 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (48.88%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,15 @@
+int maxProfit(int prices[], int size) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+    for (int i = 0; i < size; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];   
+        } else {
+            int profit = prices[i] - minPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+    }
+    return maxProfit;
+}
