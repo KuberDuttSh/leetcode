7e56f8aff7f116f129a14fbf2dcabdbb8336d8a0
Kuber Dutt Sharma <kuberduttsh@gmail.com>
2026-07-04 12:06:04 +0530

    Time: 1 ms (98.60%) | Memory: 46.8 MB (34.52%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                ans[i] = nums[i];
+            } else {
+                int index = ((i + nums[i]) % n + n) % n;
+                ans[i] = nums[index];
+            }
+        }
+
+        return ans;
+    }
+}
